9f5fe26fe48bccf741dc7df439bff2de3f11789a
Kenneth  Cassel <kennethcassel@gmail.com>
2022-09-21 11:51:55 -0500

    add script dev null

--- entrypoint.sh
+++ entrypoint.sh
@@ -81,6 +81,7 @@ if [ "${kernel}" = "" ] || [ "${dtb}" = "" ]; then
   exit 2
 fi
 echo "Booting QEMU machine \"${machine}\" with kernel=${kernel} dtb=${dtb}"
+exec script /dev/null
 exec screen -S rPi
 exec ${emulator} \
   --machine "${machine}" \
